f1de528addc9835d551ee60b5aff09cc133cd9be
Isaac Cruz <icruz@gmail.com>
2019-05-10 11:27:47 -0700

    Code Coverage Fixes

--- services/users/project/tests/test_users.py
+++ services/users/project/tests/test_users.py
@@ -5,15 +5,17 @@ from project.tests.base import BaseTestCase
 from project import db
 from project.api.models import User
 
+
 def add_user(username, email):
     user = User(username=username, email=email)
     db.session.add(user)
     db.session.commit()
     return user
 
+
 class TestUserService(BaseTestCase):
     """Tests for the Users Service"""
-    
+
     def test_users(self):
         """ Ensure the /ping route behaves correctly."""
         response = self.client.get("/users/ping")
@@ -53,7 +55,8 @@ class TestUserService(BaseTestCase):
 
     def test_add_user_invalid_json_keys(self):
         """
-        Ensure error is thrown if the JSON object object doest not have a username key.
+        Ensure error is thrown if the JSON object
+        does not have a username key.
         """
         with self.client:
             response = self.client.post(
@@ -68,7 +71,8 @@ class TestUserService(BaseTestCase):
 
     def test_add_user_duplicate_email(self):
         """
-        Ensure error is thrown if the JSON object object doest not have a username key.
+        Ensure error is thrown if the JSON object
+        does not have a username key.
         """
         with self.client:
             self.client.post(
@@ -89,7 +93,8 @@ class TestUserService(BaseTestCase):
             )
             data = json.loads(response.data.decode())
             self.assertEqual(response.status_code, 400)
-            self.assertEqual('Sorry. That email already exists.', data['message'])
+            self.assertEqual(
+                'Sorry. That email already exists.', data['message'])
             self.assertIn('fail', data['status'])
 
     def test_single_user(self):
@@ -103,7 +108,6 @@ class TestUserService(BaseTestCase):
             self.assertIn('michael@mherman.org', data['data']['email'])
             self.assertIn('success', data['status'])
 
-    
     def test_single_user_no_id(self):
         """Ensure error is thrown if the id does not exist."""
         with self.client:
@@ -121,7 +125,7 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 404)
             self.assertIn('User does not exist', data['message'])
             self.assertIn('fail', data['status'])
-    
+
     def test_all_users(self):
         """Ensure get all users behaves correctly"""
         add_user('michael', 'michael@mherman.org')
@@ -132,14 +136,17 @@ class TestUserService(BaseTestCase):
             self.assertEqual(response.status_code, 200)
             self.assertEqual(len(data['data']['users']), 2)
             self.assertIn('michael', data['data']['users'][0]['username'])
-            self.assertIn('michael@mherman.org', data['data']['users'][0]['email'])
+            self.assertIn('michael@mherman.org',
+                          data['data']['users'][0]['email'])
             self.assertIn('fletcher', data['data']['users'][1]['username'])
-            self.assertIn('letcher@notreal.com', data['data']['users'][1]['email'])
+            self.assertIn('letcher@notreal.com',
+                          data['data']['users'][1]['email'])
             self.assertIn('success', data['status'])
 
     def test_main_no_users(self):
-        """ 
-        Ensure the main route behaves correctly when no users have been added to the database.
+        """
+        Ensure the main route behaves correctly when no users
+        have been added to the database.
         """
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
@@ -147,8 +154,9 @@ class TestUserService(BaseTestCase):
         self.assertIn(b'<p>No users!</p>', response.data)
 
     def test_main_with_users(self):
-        """ 
-        Ensure the main route behaves correctly when users have been added to the database.
+        """
+        Ensure the main route behaves correctly when users
+        have been added to the database.
         """
         add_user('michael', 'michael@mherman.org')
         add_user('fletcher', 'fletcher@notreal.org')
