5f1f298d6c07bef1ac718ddf2c70954ac9ef5b5f
Isaac Cruz <isaac@padsplit.com>
2019-07-04 18:02:17 -0700

    Arbitrary Change

--- services/client/src/components/__tests__/Form.test.jsx
+++ services/client/src/components/__tests__/Form.test.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow } from "enzyme";
 import renderer from "react-test-renderer";
-import Form from "../Forms/Form";
+import Form from "../forms/Form";
 
 const testData = [
   {
