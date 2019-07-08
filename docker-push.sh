9d428cc599640a636f75aba7732841d1e903479e
Isaac Cruz <isaac@padsplit.com>
2019-07-08 10:10:14 -0700

    Settubg ebv vars

--- docker-push.sh
+++ docker-push.sh
@@ -1,44 +1,40 @@
 
-if [ -z "$CIRCLE_PULL_REQUEST" ] || [ "$CIRCLE_PULL_REQUEST" == "false" ]
+if [[ "$CIRCLE_BRANCH" == "staging" ]]; then
+    export DOCKER_ENV=stage
+elif [[ "$CIRCLE_BRANCH" == "production" ]]; then
+    export DOCKER_ENV=prod
+fi
+
+if [ "$CIRCLE_BRANCH" == "staging" ] || \
+    [ "$CIRCLE_BRANCH" == "production" ]
 then
+    curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+    unzip awscli-bundle.zip
+    ./awscli-bundle/install -b ~/bin/aws
+    export PATH=~/bin:$PATH
+    # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
+    eval $(aws ecr get-login --region us-east-1 ---no-include-email)
+    export TAG=$CIRCLE_BRANCH
+    export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
+fi
 
-    if [[ "$CIRCLE_BRANCH" == "staging" ]]; then
-        export DOCKER_ENV=stage
-    elif [[ "$CIRCLE_BRANCH" == "production" ]]; then
-        export DOCKER_ENV=prod
-    fi
-
-    if [ "$CIRCLE_BRANCH" == "staging" ] || \
-       [ "$CIRCLE_BRANCH" == "production" ]
-    then
-        curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-        unzip awscli-bundle.zip
-        ./awscli-bundle/install -b ~/bin/aws
-        export PATH=~/bin:$PATH
-        # add AWS_ACCOUNT_ID, AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY env vars
-        eval $(aws ecr get-login --region us-east-1 ---no-include-email)
-        export TAG=$CIRCLE_BRANCH
-        export REPO=$AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com
-    fi
-
-    if [ "$CIRCLE_BRANCH" == "staging" ] || \
-       [ "$CIRCLE_BRANCH" == "production" ]
-    then
-        # users
-        docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
-        docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
-        docker push $REPO/$USERS:$TAG
-        # users db
-        docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
-        docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
-        docker push $REPO/$USERS_DB:$TAG
-        # client
-        docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=TBD
-        docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
-        docker push $REPO/$CLIENT:$TAG
-        # swagger
-        docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
-        docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
-        docker push $REPO/$SWAGGER:$TAG
-    fi
+if [ "$CIRCLE_BRANCH" == "staging" ] || \
+    [ "$CIRCLE_BRANCH" == "production" ]
+then
+    # users
+    docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
+    docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
+    docker push $REPO/$USERS:$TAG
+    # users db
+    docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
+    docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
+    docker push $REPO/$USERS_DB:$TAG
+    # client
+    docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=TBD
+    docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
+    docker push $REPO/$CLIENT:$TAG
+    # swagger
+    docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
+    docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
+    docker push $REPO/$SWAGGER:$TAG
 fi
