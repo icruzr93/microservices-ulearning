b0e3335c4a6a82b46e455ee969bebeb13e071584
Isaac Cruz <isaac@padsplit.com>
2019-07-15 15:58:54 -0700

    Docker Deploy Added

--- docker-push.sh
+++ docker-push.sh
@@ -1,4 +1,5 @@
 
+
 if [ -z "$CIRCLE_PULL_REQUEST" ] || [ "$CIRCLE_PULL_REQUEST" == "false" ]
 then
 
@@ -27,14 +28,17 @@ then
         docker build $USERS_REPO -t $USERS:$COMMIT -f Dockerfile-$DOCKER_ENV
         docker tag $USERS:$COMMIT $REPO/$USERS:$TAG
         docker push $REPO/$USERS:$TAG
+
         # users db
         docker build $USERS_DB_REPO -t $USERS_DB:$COMMIT -f Dockerfile
         docker tag $USERS_DB:$COMMIT $REPO/$USERS_DB:$TAG
         docker push $REPO/$USERS_DB:$TAG
+
         # client
         docker build $CLIENT_REPO -t $CLIENT:$COMMIT -f Dockerfile-$DOCKER_ENV --build-arg REACT_APP_USERS_SERVICE_URL=$REACT_APP_USERS_SERVICE_URL
         docker tag $CLIENT:$COMMIT $REPO/$CLIENT:$TAG
         docker push $REPO/$CLIENT:$TAG
+        
         # swagger
         docker build $SWAGGER_REPO -t $SWAGGER:$COMMIT -f Dockerfile-$DOCKER_ENV
         docker tag $SWAGGER:$COMMIT $REPO/$SWAGGER:$TAG
