bbd6684c7fef341eb3a120d96314bce91dbe207b
Isaac <isaac@Isaacs-MacBook-Pro.local>
2019-09-28 13:40:31 -0500

    Cypress Tests Fixed

--- cypress/integration/index.spec.js
+++ cypress/integration/index.spec.js
@@ -6,43 +6,40 @@ const password = 'greaterthanten'
 
 describe('Index', () => {
   it('should display the page correctly if a user is not logged in', () => {
-    cy
-      .visit('/')
-      .get('h1').contains('Exercises')
-      .get('.navbar-burger').click()
-      .get('a').contains('User Status').should('not.be.visible')
-      .get('a').contains('Log Out').should('not.be.visible')
-      .get('a').contains('Register')
-      .get('a').contains('Log In')
-      .get('a').contains('Swagger')
-      .get('a').contains('Users')
-      .get('.notification.is-success').should('not.be.visible');
+    cy.visit('/');
+    cy.get('h1').contains('Exercises');
+    cy.get('.navbar-burger').click();
+    cy.get('a').contains('User Status').should('not.be.visible');
+    cy.get('a').contains('Log Out').should('not.be.visible');
+    cy.get('a').contains('Register');
+    cy.get('a').contains('Log In');
+    cy.get('a').contains('Swagger');
+    cy.get('a').contains('Users');
+    cy.get('.notification.is-success').should('not.be.visible');
   });
 
   it('should display the page correctly if a user is logged in', () => {
     cy.server();
     cy.route('POST', 'auth/register').as('createUser')
 
-    cy
-      .visit('/register')
-      .get('input[name="username"]').type(username)
-      .get('input[name="email"]').type(email)
-      .get('input[name="password"]').type(password)
-      .get('input[type="submit"]').click()
-      .wait('@createUser');
+    cy.visit('/register');
+    cy.get('input[name="username"]').type(username);
+    cy.get('input[name="email"]').type(email);
+    cy.get('input[name="password"]').type(password);
+    cy.get('input[type="submit"]').click();
+    cy.wait('@createUser');
 
-    cy
-      .get('h1').contains('Exercises')
-      .get('.navbar-burger').click()
-      .get('a').contains('User Status')
-      .get('a').contains('Log Out')
-      .get('a').contains('Register').should('not.be.visible')
-      .get('a').contains('Log In').should('not.be.visible')
-      .get('a').contains('Swagger')
-      .get('a').contains('Users')
-      .get('button').contains('Run Code')
-      .get('.notification.is-warning').should('not.be.visible')
-      .get('.notification.is-success').should('not.be.visible');
+    cy.get('h1').contains('Exercises');
+    cy.get('.navbar-burger').click();
+    cy.get('a').contains('User Status');
+    cy.get('a').contains('Log Out');
+    cy.get('a').contains('Register').should('not.be.visible');
+    cy.get('a').contains('Log In').should('not.be.visible');
+    cy.get('a').contains('Swagger');
+    cy.get('a').contains('Users');
+    cy.get('button').contains('Run Code');
+    cy.get('.notification.is-warning').should('not.be.visible');
+    cy.get('.notification.is-success').should('not.be.visible');
     
   });
 });
